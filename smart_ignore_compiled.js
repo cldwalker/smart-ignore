6860334eec077113c0078e6a4357c36662391d1a
Gabriel Horner <gabriel.horner@gmail.com>
2014-03-09 11:42:52 -0400

    handle switching workspaces

--- smart_ignore_compiled.js
+++ smart_ignore_compiled.js
@@ -15,17 +15,18 @@ goog.require('clojure.string');
 goog.require('clojure.set');
 goog.require('goog.string');
 goog.require('clojure.set');
-lt.plugins.smart_ignore.file__GT_ignore_regexs = (function file__GT_ignore_regexs(file){var parent = lt.objs.files.parent.call(null,file);var G__8247 = lt.objs.files.open_sync.call(null,file);var G__8247__$1 = (((G__8247 == null))?null:new cljs.core.Keyword(null,"content","content",1965434859).cljs$core$IFn$_invoke$arity$1(G__8247));var G__8247__$2 = (((G__8247__$1 == null))?null:((function (G__8247,G__8247__$1){
-return (function (p1__8245_SHARP_){return clojure.string.split.call(null,p1__8245_SHARP_,/\n/);
-});})(G__8247,G__8247__$1))
-.call(null,G__8247__$1));var G__8247__$3 = (((G__8247__$2 == null))?null:cljs.core.map.call(null,((function (G__8247,G__8247__$1,G__8247__$2){
+lt.plugins.smart_ignore.default_ignore_pattern = (new RegExp("(^\\..*)|\\.class$|target/|svn|cvs|\\.git|\\.pyc|~|\\.swp|\\.jar|.DS_Store|\\.nrepl-port"));
+lt.plugins.smart_ignore.file__GT_ignore_regexs = (function file__GT_ignore_regexs(file){var parent = lt.objs.files.parent.call(null,file);var G__8698 = lt.objs.files.open_sync.call(null,file);var G__8698__$1 = (((G__8698 == null))?null:new cljs.core.Keyword(null,"content","content",1965434859).cljs$core$IFn$_invoke$arity$1(G__8698));var G__8698__$2 = (((G__8698__$1 == null))?null:((function (G__8698,G__8698__$1){
+return (function (p1__8696_SHARP_){return clojure.string.split.call(null,p1__8696_SHARP_,/\n/);
+});})(G__8698,G__8698__$1))
+.call(null,G__8698__$1));var G__8698__$3 = (((G__8698__$2 == null))?null:cljs.core.map.call(null,((function (G__8698,G__8698__$1,G__8698__$2){
 return (function (relative){if(cljs.core.truth_(lt.objs.files.dir_QMARK_.call(null,lt.objs.files.join.call(null,parent,relative))))
 {return [cljs.core.str(relative),cljs.core.str("/")].join('');
 } else
 {return relative;
 }
-});})(G__8247,G__8247__$1,G__8247__$2))
-,G__8247__$2));var G__8247__$4 = (((G__8247__$3 == null))?null:cljs.core.map.call(null,goog.string.regExpEscape,G__8247__$3));return G__8247__$4;
+});})(G__8698,G__8698__$1,G__8698__$2))
+,G__8698__$2));var G__8698__$4 = (((G__8698__$3 == null))?null:cljs.core.map.call(null,goog.string.regExpEscape,G__8698__$3));return G__8698__$4;
 });
 lt.plugins.smart_ignore.update_ignore_pattern = (function update_ignore_pattern(dir){var gitignore = lt.objs.files.join.call(null,dir,".gitignore");if(cljs.core.truth_(lt.objs.files.exists_QMARK_.call(null,gitignore)))
 {var existing = clojure.string.split.call(null,lt.objs.files.ignore_pattern.source,/\|/);var new$ = lt.plugins.smart_ignore.file__GT_ignore_regexs.call(null,gitignore);var new_pattern = clojure.string.join.call(null,"|",cljs.core.sort.call(null,clojure.set.union.call(null,cljs.core.set.call(null,new$),cljs.core.set.call(null,existing))));lt.objs.files.ignore_pattern = (new RegExp(new_pattern));
@@ -34,9 +35,62 @@ return lt.objs.notifos.set_msg_BANG_.call(null,"Updated ignore-pattern.");
 {return null;
 }
 });
+lt.plugins.smart_ignore.multi_update_ignore_pattern = (function multi_update_ignore_pattern(dirs){lt.objs.files.ignore_pattern = lt.plugins.smart_ignore.default_ignore_pattern;
+var seq__8703 = cljs.core.seq.call(null,dirs);var chunk__8704 = null;var count__8705 = 0;var i__8706 = 0;while(true){
+if((i__8706 < count__8705))
+{var dir = cljs.core._nth.call(null,chunk__8704,i__8706);lt.plugins.smart_ignore.update_ignore_pattern.call(null,dir);
+{
+var G__8707 = seq__8703;
+var G__8708 = chunk__8704;
+var G__8709 = count__8705;
+var G__8710 = (i__8706 + 1);
+seq__8703 = G__8707;
+chunk__8704 = G__8708;
+count__8705 = G__8709;
+i__8706 = G__8710;
+continue;
+}
+} else
+{var temp__4092__auto__ = cljs.core.seq.call(null,seq__8703);if(temp__4092__auto__)
+{var seq__8703__$1 = temp__4092__auto__;if(cljs.core.chunked_seq_QMARK_.call(null,seq__8703__$1))
+{var c__7497__auto__ = cljs.core.chunk_first.call(null,seq__8703__$1);{
+var G__8711 = cljs.core.chunk_rest.call(null,seq__8703__$1);
+var G__8712 = c__7497__auto__;
+var G__8713 = cljs.core.count.call(null,c__7497__auto__);
+var G__8714 = 0;
+seq__8703 = G__8711;
+chunk__8704 = G__8712;
+count__8705 = G__8713;
+i__8706 = G__8714;
+continue;
+}
+} else
+{var dir = cljs.core.first.call(null,seq__8703__$1);lt.plugins.smart_ignore.update_ignore_pattern.call(null,dir);
+{
+var G__8715 = cljs.core.next.call(null,seq__8703__$1);
+var G__8716 = null;
+var G__8717 = 0;
+var G__8718 = 0;
+seq__8703 = G__8715;
+chunk__8704 = G__8716;
+count__8705 = G__8717;
+i__8706 = G__8718;
+continue;
+}
+}
+} else
+{return null;
+}
+}
+break;
+}
+});
 lt.plugins.smart_ignore.__BEH__watch_add_folder = (function __BEH__watch_add_folder(this$,path){return lt.plugins.smart_ignore.update_ignore_pattern.call(null,path);
 });
 lt.object.behavior_STAR_.call(null,new cljs.core.Keyword("lt.plugins.smart-ignore","watch-add-folder","lt.plugins.smart-ignore/watch-add-folder",3036159218),new cljs.core.Keyword(null,"reaction","reaction",4441361819),lt.plugins.smart_ignore.__BEH__watch_add_folder,new cljs.core.Keyword(null,"triggers","triggers",2516997421),new cljs.core.PersistentHashSet(null, new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"add.folder!","add.folder!",2151595160),null], null), null));
+lt.plugins.smart_ignore.__BEH__watch_workspace_select = (function __BEH__watch_workspace_select(this$){return lt.plugins.smart_ignore.multi_update_ignore_pattern.call(null,new cljs.core.Keyword(null,"folders","folders",4625622327).cljs$core$IFn$_invoke$arity$1(cljs.core.deref.call(null,lt.objs.workspace.current_ws)));
+});
+lt.object.behavior_STAR_.call(null,new cljs.core.Keyword("lt.plugins.smart-ignore","watch-workspace-select","lt.plugins.smart-ignore/watch-workspace-select",4538786176),new cljs.core.Keyword(null,"reaction","reaction",4441361819),lt.plugins.smart_ignore.__BEH__watch_workspace_select,new cljs.core.Keyword(null,"triggers","triggers",2516997421),new cljs.core.PersistentHashSet(null, new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"select!","select!",2992004631),null], null), null));
 }
 
 //# sourceMappingURL=smart_ignore_compiled.js.map
--- src/lt/plugins/smart_ignore.cljs
+++ src/lt/plugins/smart_ignore.cljs
@@ -9,6 +9,9 @@
   (:require-macros [lt.macros :refer [defui behavior]]))
 
 
+;; TODO: Ensure this saves a user-configured pattern
+(def default-ignore-pattern  (js/RegExp. "(^\\..*)|\\.class$|target/|svn|cvs|\\.git|\\.pyc|~|\\.swp|\\.jar|.DS_Store|\\.nrepl-port")  #_files/ignore-pattern)
+
 (defn file->ignore-regexs [file]
   (let [parent (files/parent file)]
     (some->> (files/open-sync file)
@@ -30,14 +33,28 @@
         (set! files/ignore-pattern (js/RegExp. new-pattern))
         (notifos/set-msg! "Updated ignore-pattern.")))))
 
+(defn multi-update-ignore-pattern [dirs]
+  (set! files/ignore-pattern default-ignore-pattern)
+  (doseq [dir dirs]
+    (update-ignore-pattern dir)))
+
 (behavior ::watch-add-folder
           :triggers #{:add.folder!}
           :reaction (fn [this path] (update-ignore-pattern path)))
 
+;; Reset and update ignore pattern when we switch workspaces.
+;; I'm doing this instead of hooking into workspace.behaviors which proved cumbersome
+;; to work with when updating the same behavior multiple times i.e. juggling diffs and
+;; behaviors not consistently reloading.
+(behavior ::watch-workspace-select
+          :triggers #{:select!}
+          :reaction (fn [this]
+                      (multi-update-ignore-pattern (:folders @workspace/current-ws))))
+
 
 (comment
   (def path "/Users/me/code/repo/bolt")
+  (file->ignore-regexs path)
   (lt.object/raise workspace/current-ws :add.folder! path)
-  (settings/safe-read (:ws-behaviors @workspace/current-ws) "workspace.behaviors")
-  (prn files/ignore-pattern)
-  (lt.objs.sidebar.workspace/open-folder))
+  (lt.object/raise workspace/current-ws :remove.folder! path)
+  (prn files/ignore-pattern))
